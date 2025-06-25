f5f13f9e7d148c91d75823c56453841d70e62808
Niti2102 <137585611+Niti2102@users.noreply.github.com>
2025-06-25 16:43:51 +0530

    Add files via upload

--- app2.py
+++ app2.py
@@ -1,18 +1,17 @@
-# rag_chatbot.py
 
-# Load Resume and Build Vector Store
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA 
 import streamlit as st
 import os
 from dotenv import load_dotenv
 load_dotenv()
 embeddings = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-v2")
-vectordb = FAISS.load_local("faissindex", embeddings, allow_dangerous_deserialization=True)
+vectordb = FAISS.load_local(".", embeddings, allow_dangerous_deserialization=True)
+
 retriever = vectordb.as_retriever()
 llm = ChatGroq(
     groq_api_key=os.environ["GROQ_API_KEY"],
